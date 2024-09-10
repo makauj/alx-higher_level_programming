1a1206b8deca6197ec700a82545984db2e39e866
makau <makauwanyoike@gmail.com>
2024-09-10 23:53:27 +0300

    reversed alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def print_alternate_reverse():
-    result = ""
-    for i in range(26):
-        if i % 2 == 0:
-            result += chr(122 - i)
-        else:
-            result += chr(89 - (i - 1))
-    print("{}".format(result), end="")
+result = ""
+for i in reverse(range(97, 123)):
+    if i % 2 == 0:
+        result += chr(i)
+    else:
+        result += chr(i - 32)
+print("{}".format(result), end="")
