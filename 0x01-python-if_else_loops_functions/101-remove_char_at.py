b12a4bea95d64a0da35ddfa09674ec5a51d68b30
makau <makauwanyoike@gmail.com>
2024-09-11 00:13:04 +0300

    copy string without n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def remove_char_at(s, n):
+def remove_char_at(str, n):
     result = ""
     c = 0
-    for i in s:
+    for i in str:
         if c == n:
             pass
         else:
-            result == c
+            result += c
         i += 1
     return result
