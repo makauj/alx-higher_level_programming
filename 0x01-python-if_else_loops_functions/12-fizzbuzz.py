a39d569805adb47c240d463e9d448eac104e4831
makau <makauwanyoike@gmail.com>
2024-09-10 22:21:30 +0300

    python FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizz_buzz():
+    for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif n % 3 == 0:
+            print("Fizz", end=" ")
+        elif n % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            if n != 99:
+                print(n, end=" ")
+            else:
+                print(n, end="")
