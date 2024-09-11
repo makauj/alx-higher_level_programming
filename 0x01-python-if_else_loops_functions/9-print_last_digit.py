e39e6fe96d1b36fd372c856bb649449815d86a8c
makau <makauwanyoike@gmail.com>
2024-09-11 13:33:58 +0300

    Last digit again

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         lastDigit = abs(number) % 10
-        print(-lastDigit, end="")
+        print(lastDigit, end="")
     else:
         lastDigit = number % 10
         print(lastDigit, end="")
