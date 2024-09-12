26c215cbc6584e3a8b28fd21a81d68db976316cf
makau <makauwanyoike@gmail.com>
2024-09-12 21:20:20 +0300

    magic?

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
 
     if a < b:
         c = add(a, b)
