b9eb1ce93369269472883c576c755a99bbf05a85
makau <makauwanyoike@gmail.com>
2024-09-12 20:19:57 +0300

    Again

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,30 @@
-#!/usr/bin/python3
 import sys
-from calculator_1 import add, sub, div, mul
+from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    op = sys.argv[2]
-
-    if op not in ops:
-        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     try:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        result = ops[op](a, b)
-        print(f"{} {} {} = {}".format(a, op, b, result))
+        operator = sys.argv[2]
+    except ValueError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {'+': add, '-': sub, '*': mul, '/': div}
+
+    if operator not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    try:
+        result = ops[operator](a, b)
+        print(f"{a} {operator} {b} = {result}")
+    except ZeroDivisionError:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
