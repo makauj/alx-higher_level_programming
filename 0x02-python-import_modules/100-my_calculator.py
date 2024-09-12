fd42ffa25f4bbd259aebe3ecde6aff89328de6d9
makau <makauwanyoike@gmail.com>
2024-09-12 21:40:51 +0300

    Correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,12 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+
 def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
 
     try:
         a = int(sys.argv[1])
@@ -17,6 +19,7 @@ def main():
 
     ops = {'+': add, '-': sub, '*': mul, '/': div}
 
+
     if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
@@ -24,6 +27,7 @@ def main():
     try:
         result = ops[operator](a, b)
         print(f"{a} {operator} {b} = {result}")
+
     except ZeroDivisionError:
         sys.exit(1)
 
