557f1dbfb5219c54961cc3588abe0c85d1e3ce97
makau <makauwanyoike@gmail.com>
2024-09-12 17:08:51 +0300

    Hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
-    import hidden_4
+    import hidden_4 as hidden
 
-    names = dir(hidden_4)
-    for name in names:
+    for name in dir(hidden):
         if name[0:2] != "__":
             print(name)
 
