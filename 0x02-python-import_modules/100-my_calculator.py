b08bea7aaf8a7d1f43bfe70345f733c1858db814
makau <makauwanyoike@gmail.com>
2024-09-12 18:28:49 +0300

    Calculator 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,17 +2,26 @@
 import sys
 from calculator_1 import add, sub, div, mul
 
-if __name__ == "__main__":
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-mycalculator.py <a> <operator> <b>")
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in ops:
+    op = sys.argv[2]
+
+    if op not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    try:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        result = ops[op](a, b)
+        print("{} {} {} = {}".format(a, op, b, result))
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
