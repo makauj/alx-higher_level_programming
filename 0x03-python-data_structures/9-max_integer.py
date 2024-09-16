089bf4b32d2f8212c48e00a3aea19e6f3cca25d2
makau <makauwanyoike@gmail.com>
2024-09-16 17:12:56 +0300

    Find max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     else:
         max_val = my_list[0]
         for i in range(len(my_list)):
-            if i > max_val:
-                max_val = i
+            if my_list[i] > max_val:
+                max_val = my_list[i]
 
         return max_val
