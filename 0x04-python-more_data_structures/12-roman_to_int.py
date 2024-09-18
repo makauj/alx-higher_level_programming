ab81e1274da88347516682635057e8688e6782ce
makau <makauwanyoike@gmail.com>
2024-09-18 15:34:56 +0300

    Roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not roman_string or not isinstamce(roman_string, str):
         return 0
     num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
