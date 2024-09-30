038c35b49c98a8bd17044034547e475e87b870ea
makau <makauwanyoike@gmail.com>
2024-09-30 19:10:14 +0300

    more lists

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return fct(*args)
+        run = fct(*args)
+        return run
     except Exception as error:
         print("Exception: {}".format(error), file-sys.stderr)
         return None
