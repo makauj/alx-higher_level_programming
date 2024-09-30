6dfae1c2f7f8bdd7e4b5904145ca9cce0e2923ba
makau <makauwanyoike@gmail.com>
2024-09-30 15:41:25 +0300

    integer list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], end=' '):
-                print("{:d}".format(my_list[i]), end=' ')
+            if isinstance(my_list[i], end=''):
+                print("{:d}".format(my_list[i]), end='')
                 count += 1
         except IndexError:
             break
