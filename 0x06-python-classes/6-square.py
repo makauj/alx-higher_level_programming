d78d71b0e46d7b976e038905e2a590b1621ae302
makau <makauwanyoike@gmail.com>
2024-10-01 16:02:07 +0300

    Square position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,4 +46,4 @@ class Square:
             print()
 
         for i in range(self.__size):
-            print(" " * self.__position + "#" * self.__size)
+            print(" " * self.__position[0] + "#" * self.__size)
