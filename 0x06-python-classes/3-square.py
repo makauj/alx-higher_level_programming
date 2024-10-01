45beb015b5796a35164de9c0da9a3bdfc9dbb069
makau <makauwanyoike@gmail.com>
2024-10-01 14:09:35 +0300

    Square area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Empty class(Does nothing)"""
 
-    def __init__(self, size="0"):
+    def __init__(self, size=0):
         """init this square class with argumnet `size`"""
 
         if not isinstance(size, int):
@@ -13,3 +13,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    
+    def area(self):
+        return (self.__size ** 2)
