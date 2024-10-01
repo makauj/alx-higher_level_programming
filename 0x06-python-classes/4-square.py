350c71d5b1a1e0f8399b50f3c4ed7280648060a4
makau <makauwanyoike@gmail.com>
2024-10-01 14:18:36 +0300

    Square set value

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Empty class to define Square"""
+
+
+class Square:
+    """Empty class(Does nothing)"""
+
+    def __init__(self, size=0):
+        """init this square class with argumnet `size`"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
