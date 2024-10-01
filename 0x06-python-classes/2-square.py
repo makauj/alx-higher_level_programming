a468f5d02088d219e6639f927c157abbe92a1958
makau <makauwanyoike@gmail.com>
2024-10-01 14:03:18 +0300

    Square exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,8 @@
 class Square:
     """Empty class(Does nothing)"""
 
-    def __init__(self, size="0")
+    def __init__(self, size="0"):
         """init this square class with argumnet `size`"""
-
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
