ac64f4731b3a341bc41d604c0b1fbb900dc14bb1
makau <makauwanyoike@gmail.com>
2024-10-01 13:50:17 +0300

    Square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Empty class to define Square"""
+
+
+class Square:
+    """Empty class(Does nothing)"""
+
+    def __init__(self, size="0"):
+        """init this square class with argumnet `size`"""
+        self._size = size
