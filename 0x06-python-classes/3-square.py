d1794f11057764bad3da08215fb609085f898849
makau <makauwanyoike@gmail.com>
2024-10-01 14:04:45 +0300

    Square exceptions

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Empty class(Does nothing)"""
 
-    def __init__(self, size=0):
+    def __init__(self, size="0"):
         """init this square class with argumnet `size`"""
 
         if not isinstance(size, int):
