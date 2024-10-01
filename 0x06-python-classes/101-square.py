323bce9cfcafe71871ec7fda211b321f8d7c5a65
makau <makauwanyoike@gmail.com>
2024-10-01 19:52:58 +0300

    Singly linked list

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Class to define Square"""
+
+
+class Square:
+    """Defines a square by size and position."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the square with size and position."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character # at the given position."""
+        if self.__size == 0:
+            print()
+            return
+
+        for i in range(self.__position[1]):
+            print()
+
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+    
+    def __str__(self):
+        output = ""
+        if self.size == 0:
+            return output
+        for i in range(self.__position[1]):
+            output += "\n"
+        for i in range(self.__size):
+            output += " " * self.__position[0] + "#" * self.__size + "\n"
+
+        return output.rstrip()
