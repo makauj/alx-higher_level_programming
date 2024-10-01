554d9ce1429c8c262ce3ed2673926409573d8c51
makau <makauwanyoike@gmail.com>
2024-10-01 14:01:33 +0300

    Square exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Empty class to define Square"""
+
+
+class Square:
+    """Empty class(Does nothing)"""
+
+    def __init__(self, size="0")
+        """init this square class with argumnet `size`"""
+
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
