9f3179083103033dd90a36090c430d204d214206
makau <makauwanyoike@gmail.com>
2024-10-01 15:38:36 +0300

    Square position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,9 +18,6 @@ class Square:
     def size(self):
         return self.__size
 
-    def position(self):
-        return self.__position
-
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -28,6 +25,10 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
 
     @position.setter
     def position(self, value):
