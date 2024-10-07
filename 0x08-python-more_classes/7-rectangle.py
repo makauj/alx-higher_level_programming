ec0d12e263671cd9773d45c80303635732669305
makau <makauwanyoike@gmail.com>
2024-10-07 20:27:02 +0300

    ___

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,6 +7,7 @@ include private instance attributes width and height
 class Rectangle:
     """instantiate width and height"""
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.width = width
