a5f9b0a05e5329079c3cf9a7f2a8011b114ae204
makau <makauwanyoike@gmail.com>
2024-10-07 16:52:37 +0300

    Added sides

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,14 +8,8 @@ class Rectangle:
     """instantiate width and height"""
     def __init__(self, width=0, height=0):
 
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
