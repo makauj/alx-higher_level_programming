5387121f2a47d6fbd988739783046368b1e612e9
makau <makauwanyoike@gmail.com>
2024-10-08 16:11:32 +0300

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,9 +2,7 @@
 
 
 def add_integer(a, b=98):
-    """
-    
-    """
+    """function that adds two numbers"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
