9938c0de69f8fbaae47c8e5a49bf8f5c5c85f611
makau <makauwanyoike@gmail.com>
2024-10-14 22:46:18 +0300

    docstring

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""function that checks if obj is exactly an instance of a_class"""
 
 
 def is_same_class(obj, a_class):
+    """function code"""
     if type(obj) is a_class:
         return True
     else:
