6ba7983c735e64dedd32bd456f06b85275de1689
makau <makauwanyoike@gmail.com>
2024-10-14 22:58:14 +0300

    correction

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """function that checks if obj is an instance of an inherited class"""
 
+
 def is_kind_of_class(obj, a_class):
     """
     if object is an instance of an inherited class,
