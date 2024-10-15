e018d1f1b1229ceb697e73c2de4b0f820c5a9975
makau <makauwanyoike@gmail.com>
2024-10-15 14:09:36 +0300

    correction

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """function to open a file and print its contents"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Write a string to a UTF* text file"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
