4cc71a232cde06a7a2dcebcc15d427bb79077e36
makau <makauwanyoike@gmail.com>
2024-10-15 14:20:47 +0300

    typos

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        items = load_from_json_file("add_item.jsom")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.jsom")
+    save_to_json_file(items, "add_item.json")
