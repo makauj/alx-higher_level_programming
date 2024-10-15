714fce74ed559620da4213952b54a0aad3f0864f
makau <makauwanyoike@gmail.com>
2024-10-15 14:07:11 +0300

    pycodestyle

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Simple implementation of a serialization and deselialization mechanism
 (concept of representation of an object to another format, without
-losing any information and allow us to rebuild an object based on this 
+losing any information and allow us to rebuild an object based on this
 representation)
 """
 
@@ -20,7 +20,7 @@ class Student:
         if type(attrs) is list and all(type(ele) is str for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-    
+
     def reload_from_json(self, json):
         """
         replace all attributes of a student instance
