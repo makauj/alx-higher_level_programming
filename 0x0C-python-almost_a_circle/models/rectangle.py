072699fe3ec9a494047c225eabdda662c85d9520
makau <makauwanyoike@gmail.com>
2024-10-17 13:00:30 +0300

    docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ class Rectangle(Base):
     """Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize rectangle"""
         self.__width = width
         self.__height = height
         self.__x = x
@@ -15,10 +16,12 @@ class Rectangle(Base):
     
     @property
     def width(self):
+        """get value of width"""
         return self.__width
     
     @width.setter
     def width(self, value):
+        """set value of width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -27,10 +30,12 @@ class Rectangle(Base):
     
     @property
     def height(self):
+        """get value of height"""
         return self.__height
     
     @height.setter
     def height(self, value):
+        """set value of height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -39,10 +44,12 @@ class Rectangle(Base):
     
     @property
     def x(self):
+        """get value of x"""
         return self.__x
     
     @x.setter
     def x(self, value):
+        """set value for x"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -51,10 +58,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """get value of y"""
         return self.__y
     
     @y.setter
     def y(self, value):
+        """"set value of y"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -62,9 +71,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """return area of rectangle"""
         return self.__height * self.__width
     
     def display(self):
+        """print rectangle using #"""
         for _ in range(self.__height):
             for x in range(self.x):
                 print(" ", end="")
@@ -73,5 +84,21 @@ class Rectangle(Base):
             print()
     
     def __str__(self):
+        """
+        define the format of the string representation of a rectange
+        """
         return (f"[Rectangle] ({self.__id})
                 {self.__x} / {self.__y} - {self.__width}/{self.__height}")
+    
+    def update(self, *args):
+        """public method to assign an arg to each attribute"""
+        if len(args) >= 1:
+            self.id >= args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
