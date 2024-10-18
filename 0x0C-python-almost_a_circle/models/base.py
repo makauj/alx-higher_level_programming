ea1f781f92612d917a29396f5bfe13bb7d65858b
makau <makauwanyoike@gmian.com>
2024-10-18 04:45:03 +0300

    json dump

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """base class"""
+import json
 
 
 class Base:
@@ -8,8 +9,16 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """initialize the Base class"""
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """static method that returns JSON string"""
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
