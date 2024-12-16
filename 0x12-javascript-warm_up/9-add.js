6828a08e0a23044d4bdb38e22966aa353568a9ee
makau <makauwanyoike@gmail.com>
2024-12-16 16:09:39 +0300

    fix my code

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,8 +6,4 @@ function add (a, b) {
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-if (isNaN(num2) || isNaN(num1)) {
-  console.log('Missing or invalid number');
-} else {
-  console.log(add(num1, num2));
-}
+console.log(add(num1, num2));
