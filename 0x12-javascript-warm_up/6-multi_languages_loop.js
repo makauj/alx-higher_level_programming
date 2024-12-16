a4318ab13a092bb91156bfeb7c053303f257fa9b
makauj <makauwanyoike@gmail.com>
2024-12-16 11:29:44 +0300

    multiline usingloop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+/* 
+for (let i = 0; i < arr.length; i++)
+{
+    console.log(arr[i]);
+}
+
+let l = 0;
+while (l < arr.length)
+{
+    console.log(arr[l]);
+    l++;
+}
+*/
+arr.forEach(arr => console.log(arr));
