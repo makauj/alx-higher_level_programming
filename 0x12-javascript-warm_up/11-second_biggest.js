1ef8b8eb5f9dec76a82f0ed0c92462627cb21694
makauj <makauwanyoike@gmail.com>
2024-12-16 14:35:54 +0300

    sort, change and add

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num = args.map(arg => parseInt(arg));
+
+if (num.length < 2) {
+    console.log(0);
+} else {
+    const sortedNum = [...new Set(num)].sort((a, b) => b - a);
+    console.log(sortedNum[1] || 0);
+}
