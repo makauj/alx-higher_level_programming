23ab78c55d13fdc3f222773b88e5f5547cd010d3
makauj <makauwanyoike@gmail.com>
2024-12-16 12:01:58 +0300

    addition

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function add(a, b)
+{
+    return a + b;
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (isNaN(num2) || isNaN(num2))
+{
+    console.log('Missing or invalid number');
+}
+else
+{
+    console.log(add(num1, num2));
+}
