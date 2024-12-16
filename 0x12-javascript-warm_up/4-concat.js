1cbd89fc5d0b7536eca22e75156d603a1342e694
makauj <makauwanyoike@gmail.com>
2024-12-16 09:56:49 +0300

    concatenate

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+let myVar = 'is';
+if (args.length === 0)
+    {
+        args[0] = 'undefined';
+        args[1] = 'undefined';
+    }
+console.log(args[0], myVar, args[1]);
