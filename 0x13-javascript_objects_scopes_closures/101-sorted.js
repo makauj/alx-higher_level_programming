d9c4280a455990bd56f4fdc292406938cdbc1768
makau <makauwanyoike@gmail.com>
2024-12-19 17:00:17 +0300

    semistandard

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,11 +4,10 @@ const dict = require('./101-data.js').dict;
 const newDict = {};
 
 Object.getOwnPropertyNames(dict).forEach(occurences => {
-  const occurences = dict[userId].length;
   if (newDict[dict[occurences]] === undefined) {
-    newDict[occurences] = [parseInt(userId)];
+    newDict[dict[occurences]] = [occurences];
   } else {
-    newDict[occurences].push(parseInt(userId));
+    newDict[dict[occurences]].push(occurences);
   }
 });
 console.log(newDict);
