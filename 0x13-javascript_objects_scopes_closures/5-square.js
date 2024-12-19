c7e477f927a9e741cc76e07cc12e22cfbc2c20d8
makau <makauwanyoike@gmail.com>
2024-12-19 15:03:59 +0300

    better squares

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,36 +1,11 @@
 #!/usr/bin/node
 
-class Rectangle {
-    constructor(w, h) {
-        if ((w < 1 && Number.isInteger(w)) || h < 0 && Number.isInteger(h)) {
-            this.height = h;
-            this.width = w;
-        } else {
-            return {};
-        }
-    }
-    print() {
-        if (this.width && this.height) {
-            for (let i = 0; i < this.height; i++) {
-                console.log('X'.repeat(this.width));
-            }  
-        }
-    }
-    rotate() {
-        if (this.width && this.height) {
-            [this.width, this.height] = [this.height, this.width];
-        }
-    }
-    double() {
-        if (this.width && this.height) {
-            this.width *= 2;
-            this.height *= 2;
-        }
-    }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
     constructor(size) {
         super(size, size);
     }
 }
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const PrevSquare = require('./5-square');
+
+class Square extends PrevSquare {
+    charPrint(c) {
+        const char = c || 'X';
+        for (let i = 0; i < this.size; i++) {
+            console.log(char.repeat(this.width));
+        }
+    }
+}
+
+module.exports = Square;
