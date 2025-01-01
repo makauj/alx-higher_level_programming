4e20af3e7eb4e1ff91d7b3c1a50f108631218c23
makau <makauwanyoike@gmail.com>
2025-01-01 23:55:11 +0300

    again?

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,12 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     new_db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
     cursor = new_db.cursor()
     cursor.execute("SELECT * FROM states")
