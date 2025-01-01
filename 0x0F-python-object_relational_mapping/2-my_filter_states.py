c5ff67d3a8c5a5ac49d46d093d2ac8ebceac1bec
makau <makauwanyoike@gmail.com>
2025-01-01 16:05:41 +0300

    make code more readable

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 """
-script that filters states matching given argument
+Script that takes in an argument and displays all
+states where the name matches the argument
 """
 
-from sys import argv
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-                   .format(argv[4]))
-    query_rows = cursor.fetchall()
-    for row in query_rows:
-        print(row)
+    q = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4])
+    cursor.execute(q)
+
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
     cursor.close()
     db.close()
