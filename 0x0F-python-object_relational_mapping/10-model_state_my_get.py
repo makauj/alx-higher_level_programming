e27a97a3a1e78ccf79642f9266cbd06d6dd80e1b
makau <makauwanyoike@gmail.com>
2025-01-01 22:45:11 +0300

    ??

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     state = session.query(State).filter(State.name == state_name).first()
 
     if state:
-        print(state.id)
+        print(str(state.id))
     else:
         print("Not found")
 
