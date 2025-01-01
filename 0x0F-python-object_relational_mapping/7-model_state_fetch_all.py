5ab59d929347b687528d0d9d3617d408c7638886
makau <makauwanyoike@gmail.com>
2025-01-01 20:55:02 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,10 +11,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
 
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost/{db}')
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
 
     Base.metadata.create_all(engine)
 
