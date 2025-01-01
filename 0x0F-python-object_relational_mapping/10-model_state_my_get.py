2ed40b07db1cfa4863a0091b9ffd7ecccb1feb22
makau <makauwanyoike@gmail.com>
2025-01-01 22:39:09 +0300

    print state with name passed in argument

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that prints the State object with the name passed as an argument from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessiomaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(f'mysql+mysqldb://{usr}:{passwd}@localhost/{db}')
+
+    Base.metadata.create_all(engine)
+
+    Session = sessiomaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
