2288ad9f61ae35f62d8daba0984655409c15a7e1
makau <makauwanyoike@gmail.com>
2025-01-01 20:38:05 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    q = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4])
+    q = f"SELECT * FROM states WHERE name = '{argv[4]}' ORDER BY id ASC"
     cursor.execute(q)
 
     states = cursor.fetchall()
--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,20 +10,20 @@ from sqlalchemy import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    
+
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
     engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost/{db}')
-    
+
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     states = session.query(State).order_by(State.id).all()
-    
+
     for state in states:
         print(f'{state.id}: {state.name}')
-    
+
     session.close()
