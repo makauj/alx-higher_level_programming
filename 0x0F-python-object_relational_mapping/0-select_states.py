76b38dd9631282c29d7cd8a5229f8c992ef765e9
makau <makauwanyoike@gmail.com>
2025-01-01 23:51:46 +0300

    a correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+    new_db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=argv[1],
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         db=argv[3]
     )
 
-    cursor = db.cursor()
+    cursor = new_db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     states = cursor.fetchall()
@@ -23,4 +23,4 @@ if __name__ == "__main__":
         print(state)
 
     cursor.close()
-    db.close()
+    new_db.close()
