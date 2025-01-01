5c6d7adf52cbdd017b30627a4fba04e02b8266a8
makau <makauwanyoike@gmail.com>
2025-01-01 22:43:48 +0300

    correction

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,7 +5,6 @@ argument from the database hbtn_0e_6_usa
 """
 
 import sys
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
