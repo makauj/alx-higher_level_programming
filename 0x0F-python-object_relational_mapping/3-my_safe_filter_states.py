48b622b96ed167cc735f6c8c043d1f1b4dd68770
makau <makauwanyoike@gmail.com>
2025-01-01 11:03:26 +0300

    filter by states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,10 +12,14 @@ if __name__ == "__main__":
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
+
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                   .format(argv[4]))
+
     query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
+
     cursor.close()
     db.close()
