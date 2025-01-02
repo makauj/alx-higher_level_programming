516ba57b5708d3ae3a61146d7d2a0cd6edee539e
makau <makauwanyoike@gmail.com>
2025-01-02 07:41:30 +0300

    fetchball

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,16 +8,18 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
     cursor = db.cursor()
-    cursor.execute(f"SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    states = cursor.fetchball()
+    states = cursor.fetchall()
 
     for state in states:
         if state[1][0] == 'N':
