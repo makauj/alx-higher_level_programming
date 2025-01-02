20ef76a5d8d20df70dd628371a8454cb95db98c6
makau <makauwanyoike@gmail.com>
2025-01-02 08:13:48 +0300

    localhost

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,7 @@ from model_state import Base, State
 if __name__ == "__main__":
 
     engine = create_engine(
-        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
 
     Base.metadata.create_all(engine)
 
