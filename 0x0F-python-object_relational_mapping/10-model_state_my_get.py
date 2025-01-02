1b3bf83842f83ef64d35257dce830431d84c4adf
makau <makauwanyoike@gmail.com>
2025-01-02 08:42:58 +0300

    code

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,24 +4,21 @@ Script that prints the State object with the name passed as an
 argument from the database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    usr = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-
-    engine = create_engine(f'mysql+mysqldb://{usr}:{passwd}@localhost/{db}')
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
+        @localhost:3306/{argv[3]}')
 
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter_by(state_name=sys.argv[4]).first()
+    state = session.query(State).filter_by(state_name=argv[4]).first()
 
     if state is not None:
         print(str(state.id))
