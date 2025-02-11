afa4a2dc48c3e05c0b45dfd6bc66fdeb384d04d2
makau <makauwanyoike@gmail.com>
2025-02-12 02:51:13 +0300

    complete

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const tasks = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (tasks[todo.userId]) {
+        tasks[todo.userId] += 1;
+      } else {
+        tasks[todo.userId] = 1;
+      }
+    }
+  });
+    console.log(tasks);
+});
