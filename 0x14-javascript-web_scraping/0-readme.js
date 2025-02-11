27d47df2f900fc70b1c5058f5759709956aa08f3
makau <makauwanyoike@gmail.com>
2025-02-12 02:46:21 +0300

    changed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filename = ProcessingInstruction.argv(2);
+const filename = process.argv(2);
 fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
