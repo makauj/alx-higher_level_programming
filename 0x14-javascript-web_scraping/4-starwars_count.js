5e19a57e88632f2b708b8491f1c596fdfce7220c
makau <makauwanyoike@gmail.com>
2025-02-12 02:37:11 +0300

    count starwars

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+
+if (process.argv.length > 2) {
+  request(process.argv[2], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else if (body) {
+        const filmsChar = JSON.parse(body).results.filter(
+            x => x.characters.find(y => y.match(/\18\/?$/))
+        );
+        console.log(filmsChar.length);
+    }
+  });
+}
