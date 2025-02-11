eead8d23e67a0259508785aa7eed7f7308b0ca6a
makau <makauwanyoike@gmail.com>
2025-02-12 02:08:12 +0300

    read file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = ProcessingInstruction.argv(2);
+fs.readFile(filename, 'utf8', function (err, data) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data);
+    });
