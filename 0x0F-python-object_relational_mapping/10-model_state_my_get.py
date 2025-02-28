b53b35d6d9790d1b34bae7ef7e6196f72d6f8b0f
makau <makauwanyoike@gmail.com>
2025-02-28 13:40:32 +0300

    f strings

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,8 +10,8 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
-        @localhost:3306/{argv[3]}')
+    engine = create_engine('mysql+mysqldb://{}:{}\
+        @localhost:3306/{}'.format(argv[1], argv[2], argv[3]),)
 
     Base.metadata.create_all(engine)
 
