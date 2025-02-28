83dcbd222b4cc5a7260a6abbc225c56e6b171b77
makau <makauwanyoike@gmail.com>
2025-02-28 15:11:07 +0300

    sorting keys

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
+// import a dictionary of occurrences by userid and compute a new dictionary
+// of user ids by occurrences
 const dict = require('./101-data.js').dict;
 
 const newDict = {};
 
-for (const [key, val] of Object.entries(dict)) {
-  newDict[val] ? newDict[val].push(key) : (newDict[val] = [key]);
-}
-console.log(newDict);
+Object.getOwnPropertyNames(dict).forEach((key) => {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  }
+  newDict[dict[key]].push(key);
+});
