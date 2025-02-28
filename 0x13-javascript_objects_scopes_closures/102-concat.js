a563f6d24a3e56d3258be6ed6873c63ba9850a24
makau <makauwanyoike@gmail.com>
2025-02-28 15:05:56 +0300

    concat 2 files with JS

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,36 +5,21 @@ const fs = require('fs');
 // Get the file paths from the command-line arguments
 const [fileA, fileB, fileC] = process.argv.slice(2);
 
-// Check if all arguments are provided
-if (!fileA || !fileB || !fileC) {
-  console.log('Usage: node concat-files.js <source1> <source2> <destination>');
-  process.exit(1);
+// Check if the files exist and are files
+if (
+  fs.existsSync(fileA) &&
+  fs.statSync(fileA).isFile() &&
+  fs.existsSync(fileB) &&
+  fs.statSync(fileB).isFile() &&
+  fileC !== undefined
+) {
+  // Read the contents of the files and write them to the third file
+  const dataA = fs.readFileSync(fileA, 'utf8');
+  const dataB = fs.readFileSync(fileB, 'utf8');
+  const stream = fs.createWriteStream(fileC);
+
+  // Write the contents of the files to the third file
+  stream.write(dataA);
+  stream.write(dataB);
+  stream.end();
 }
-
-// Read the contents of the first source file
-fs.readFile(fileA, 'utf8', (err, data1) => {
-  if (err) {
-    console.error(`Error reading ${fileA}:`, err);
-    return;
-  }
-
-  // Read the contents of the second source file
-  fs.readFile(fileB, 'utf8', (err, data2) => {
-    if (err) {
-      console.error(`Error reading ${fileB}:`, err);
-      return;
-    }
-
-    // Concatenate the data from both files
-    const concatenatedData = data1 + data2;
-
-    // Write the concatenated data to the destination file
-    fs.writeFile(fileC, concatenatedData, (err) => {
-      if (err) {
-        console.error(`Error writing to ${fileC}:`, err);
-        return;
-      }
-      console.log(`Successfully concatenated ${fileA} and ${fileB} into ${fileC}`);
-    });
-  });
-});
